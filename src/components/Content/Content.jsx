a663eacf5f545f1788994e8abba264a337eb0c86
Theo02-12 <mrcouvelard@gmail.com>
2023-04-23 17:37:48 +0200

    title et icones

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Content.css'
 
+import Icons from '../IconsSkill/Icons'
+
 const Content = () => {
     return (
-        <div>
-            content
+        <div className='content-name-infos'>
+            <div className='title'>
+                <h1>Bonjour, je m'appelle Théo et je suis</h1>
+                <h2>Développeur web et web mobile</h2>
+                <h3>Je suis à la recherche d'un stage.</h3>
+            </div>
+            <Icons />
         </div>
+
+        
     )
 };
 
---
+++ src/components/IconsSkill/Icons.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './Icons.css';
+
+const Icons = () => {
+    return(
+    <div className="icons-skill">
+        <span>
+            <i class="fa-solid fa-code"></i>
+            <h3>front-end</h3>
+        </span>
+        <span>
+            <i class="fa-solid fa-server"></i>
+            <h3>back-end</h3>
+        </span>
+        <span>
+            <i class="fa-solid fa-mobile-screen-button"></i>
+            <h3>responsive</h3>
+        </span>
+        <span>
+            <i class="fa-solid fa-pen"></i>
+            <h3>maquettage</h3>
+        </span>
+    </div>
+    )
+}
+export default Icons
